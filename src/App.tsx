226d5da28de3a7087ba752e4a36e7b94efb9b898
vf12776-ux <vf12776@gmail.com>
2025-11-14 05:01:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-// src/App.tsx
-import React from 'react'
-import './App.css'
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Courier App</h1>
+    <div>
+      <h1>Courier App работает</h1>
       <p>Заказы будут здесь</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
