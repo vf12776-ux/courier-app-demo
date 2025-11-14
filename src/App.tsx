22dc06f74749bc52314c7553a87112cc1c591753
vf12776-ux <vf12776@gmail.com>
2025-11-14 04:36:29 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Courier App - Демо версия</h1>
+      <p>Функционал заказов и уведомлений в разработке</p>
+    </div>
+  )
+}
+
+export default App
