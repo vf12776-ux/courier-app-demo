0907617c5222e2fc44083a6ff36991820ac9d724
vf12776-ux <vf12776@gmail.com>
2025-11-14 04:58:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/courier-app/', // добавь эту строку
   server: {
     port: 3000,
     host: true
